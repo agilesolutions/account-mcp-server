7fd1522ff5e0748aa3dc4b37544d032fa6a95e40
admin <rong@agile-solutions.ch>
2025-04-25 14:24:08 +0200

    fixes

--- src/main/java/com/agilesolutions/config/McpConfig.java
+++ src/main/java/com/agilesolutions/config/McpConfig.java
@@ -20,18 +20,4 @@ public class McpConfig {
                 .build();
     }
 
-    @Bean
-    public List<McpServerFeatures.SyncPromptRegistration> prompts() {
-        var prompt = new McpSchema.Prompt("persons-by-nationality", "Get persons by nationality",
-                List.of(new McpSchema.PromptArgument("nationality", "Person nationality", true)));
-
-        var promptRegistration = new McpServerFeatures.SyncPromptRegistration(prompt, getPromptRequest -> {
-            String nameArgument = (String) getPromptRequest.arguments().get("name");
-            var userMessage = new McpSchema.PromptMessage(McpSchema.Role.USER,
-                    new McpSchema.TextContent("How many persons are from " + nameArgument + " ?"));
-            return new McpSchema.GetPromptResult("Count persons by nationality", List.of(userMessage));
-        });
-
-        return List.of(promptRegistration);
-    }
 }
